df1e5a92f43e10e9891fbf873ed7e29c0c5b6d3f
Boris <boris.nesterko@hotmail.com>
2014-03-14 16:43:45 +0200

    Initial commit.

---
+++ Thinktecture.Domain/Security/CustomAuthorizationManager.cs
@@ -0,0 +1,8 @@
+﻿using System.Security.Claims;
+
+namespace Thinktecture.Domain.Security
+{
+    public class CustomAuthorizationManager : ClaimsAuthorizationManager
+    {
+    }
+}
---
+++ Thinktecture.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Thinktecture.Website.WebApiApplication" Language="C#" %>
